0e923a0c349685a5a6a3c5d8603ad701cd0fa7fc
Shane Weng <ac2k888@yahoo.com>
2014-11-21 20:17:22 -0800

    add color for year.

--- day_chain.rb
+++ day_chain.rb
@@ -1,8 +1,16 @@
 require 'date'
 
+class String
+  def black;          "\033[30m#{self}\033[0m" end
+  def red;            "\033[31m#{self}\033[0m" end
+  def green;          "\033[32m#{self}\033[0m" end
+  def brown;          "\033[33m#{self}\033[0m" end
+  def blue;           "\033[34m#{self}\033[0m" end
+end
+
 time = DateTime.now
 
-puts "\n  #{time.year}\n\n"
+puts "\n  #{time.year}\n\n".brown
 out = ->(a) { (1..a).each { |d| print d, ' ' }}
 (1..12).each do |m|
   case m
